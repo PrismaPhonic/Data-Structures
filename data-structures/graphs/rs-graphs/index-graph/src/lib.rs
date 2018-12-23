e1a18004c03e70ec14b07e05c15511d4b83c74ca
Peter Farr <Peter@PrismaPhonic.com>
2018-12-22 22:36:36 -0800

    Added shortest path finding method that uses bfs to find shortest path between two nodes and added benchmark test to check performance of new method as well as unit testing for the method

--- data-structures/graphs/rs-graphs/index-graph/src/lib.rs
+++ data-structures/graphs/rs-graphs/index-graph/src/lib.rs
@@ -1,4 +1,9 @@
+#![feature(test)]
 use std::collections::HashMap;
+use std::collections::HashSet;
+use std::collections::VecDeque;
+
+extern crate test;
 
 /**
  * Non-directional graph representation with a memory arena
@@ -20,8 +25,6 @@ struct Node<T> {
     adjacent: Vec<usize>,
 }
 
-// We get the next free index and, push a fresh node with
-// data supplied to our Graph and then return the index
 impl<T> Graph<T> {
     pub fn new() -> Graph<T> {
         let nodes: HashMap<usize, Node<T>> = HashMap::new();
@@ -38,10 +41,13 @@ impl<T> Graph<T> {
 
     // is a an edge of b? this is a one directional search only and works
     // if the edge we are checking for is a node that has been deleted
+    // for right row this is just for unit testing
     pub fn is_edge_of(&self, a: usize, b: usize) -> bool {
         self.nodes.get(&b).unwrap().adjacent.contains(&a)
     }
 
+    // We get the next free index and, push a fresh node with
+    // data supplied to our Graph and then return the index
     pub fn new_node(&mut self, data: T) -> usize {
         let index = self.next_index;
 
@@ -60,7 +66,7 @@ impl<T> Graph<T> {
         }
 
         // search through all nodes adjaceny lists to see if index
-        // existed inn any of them
+        // existed in any of them and remove
         for (_k, v) in self.nodes.iter_mut() {
             if v.adjacent.contains(&n) {
                 v.adjacent.remove(n);
@@ -91,6 +97,39 @@ impl<T> Graph<T> {
             return true;
         }
     }
+
+    
+    pub fn shortest_path(&self, start: usize, end: usize) -> i32 {
+        let mut queue = VecDeque::new();
+        let first = vec![start];
+        queue.push_back(first);
+        let mut visited = HashSet::new();
+        visited.insert(start);
+        let mut count = 0;
+
+        loop {
+            let curr_group = queue.pop_front().unwrap();
+
+            let mut next_group: Vec<usize> = vec![];
+            for curr_index in curr_group {
+                if curr_index == end {
+                    return count;
+                }
+
+                let adjacent = &self.nodes.get(&curr_index).unwrap().adjacent;
+                adjacent.iter().for_each(|neighbor| {
+                    if !visited.contains(neighbor) {
+                        next_group.push(*neighbor);
+                        visited.insert(*neighbor);
+                    }
+                });
+            }
+            if !next_group.is_empty() {
+                queue.push_back(next_group);
+            }
+            count += 1;
+        }
+    }
 }
 
 
@@ -98,6 +137,7 @@ impl<T> Graph<T> {
 #[cfg(test)]
 mod tests {
     use super::*;
+    use test::Bencher;
 
     #[test]
     fn add_vertex() {
@@ -142,5 +182,98 @@ mod tests {
 
         assert_eq!(graph.contains(a), false);
         assert_eq!(graph.is_edge_of(a, b), false);
+    }
+
+    #[test]
+    fn shortest_path() {
+        let mut graph = Graph::new(); 
+        let s = graph.new_node('S');
+        let p = graph.new_node('P');
+        let u = graph.new_node('U');
+        let x = graph.new_node('X');
+        let q = graph.new_node('Q');
+        let y = graph.new_node('Y');
+        let v = graph.new_node('V');
+        let r = graph.new_node('R');
+        let w = graph.new_node('W');
+        let t = graph.new_node('T');
+        
+        graph.link_nodes(s, p);
+        graph.link_nodes(s, u);
+
+        graph.link_nodes(p, x);
+        graph.link_nodes(u, x);
+        
+        graph.link_nodes(p, q);
+        graph.link_nodes(u, v);
+        
+        graph.link_nodes(x, q);
+        graph.link_nodes(x, y);
+        graph.link_nodes(x, v);
+
+        graph.link_nodes(q, r);
+        graph.link_nodes(y, r);
+        
+        graph.link_nodes(y, w);
+        graph.link_nodes(v, w);
+        
+        graph.link_nodes(r, t);
+        graph.link_nodes(w, t);
+
+        assert_eq!(
+            graph.shortest_path(p, v),
+            2
+        );
+        assert_eq!(
+            graph.shortest_path(p, y),
+            2
+        );
+        assert_eq!(
+            graph.shortest_path(p, w),
+            3
+        );
+        assert_eq!(
+            graph.shortest_path(s, t),
+            4
+        );
+    }
+    
+    #[bench]
+    fn bench_shortest_path(b: &mut Bencher) {
+        let mut graph = Graph::new(); 
+        let s = graph.new_node('S');
+        let p = graph.new_node('P');
+        let u = graph.new_node('U');
+        let x = graph.new_node('X');
+        let q = graph.new_node('Q');
+        let y = graph.new_node('Y');
+        let v = graph.new_node('V');
+        let r = graph.new_node('R');
+        let w = graph.new_node('W');
+        let t = graph.new_node('T');
+        
+        graph.link_nodes(s, p);
+        graph.link_nodes(s, u);
+
+        graph.link_nodes(p, x);
+        graph.link_nodes(u, x);
+        
+        graph.link_nodes(p, q);
+        graph.link_nodes(u, v);
+        
+        graph.link_nodes(x, q);
+        graph.link_nodes(x, y);
+        graph.link_nodes(x, v);
+
+        graph.link_nodes(q, r);
+        graph.link_nodes(y, r);
+        
+        graph.link_nodes(y, w);
+        graph.link_nodes(v, w);
+        
+        graph.link_nodes(r, t);
+        graph.link_nodes(w, t);
+
+        b.iter(|| graph.shortest_path(s, t));
     }
 }
