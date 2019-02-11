0bcabb7f1b201d799375a104214e8df0b9cc1e4a
Peter Farr <Peter@PrismaPhonic.com>
2019-02-10 20:21:19 -0800

    Added example of BFS iterative to tree data structure

--- data-structures/trees/simple-rust/binary-tree-simple/src/lib.rs
+++ data-structures/trees/simple-rust/binary-tree-simple/src/lib.rs
@@ -16,7 +16,64 @@ impl<T> Tree<T> {
 
 type TreeNode<T> = Option<Box<Tree<T>>>;
 
-type TreeLevel<T> = Vec<Option<Box<Tree<T>>>>;
+type TreeLevel<'a, T> = Vec<&'a Option<Box<Tree<T>>>>;
+
+
+/// Simple test to show BFS tree level by tree level approach iterative
+///
+use std::collections::VecDeque;
+
+fn isTreeSymmetric(t: TreeNode<i32>) -> bool {
+    let mut deque = VecDeque::new();
+
+    deque.push_back(vec![&t]);
+
+    while !deque.is_empty() {
+        let level = deque.pop_front().unwrap();
+
+        // check if current level is symmetric
+        if !is_level_symmetric(&level) {
+            return false;
+        }
+
+        // push children of all nodes at this level into next_level vec
+        let mut next_level = Vec::new();
+        for node in level {
+            if let Some(n) = node {
+                next_level.push(&n.left);
+                next_level.push(&n.right);
+            }
+        }
+
+        if !next_level.is_empty() {
+            deque.push_back(next_level);
+        }
+    }
+
+    true
+}
+
+fn is_level_symmetric(level: &TreeLevel<i32>) -> bool {
+    for i in 0..level.len() / 2 {
+        if let Some(l_node) = level[i] {
+            if let Some(r_node) = level[level.len() - 1 - i] {
+                if l_node.val != r_node.val {
+                    return false;
+                }
+            } else {
+                return false;
+            }
+        } else {
+            if let Some(_) = level[level.len() - 1 - i] {
+                return false;
+            }
+        }
+    }
+
+    true
+}
+
+
 
 #[cfg(test)]
 mod tests {
